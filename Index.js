e254dba395e71805d64bf754b3546e7d5f2c14a1
Andrew54757 <andrews54757@gmail.com>
2016-10-06 11:59:05 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,10 +1,9 @@
 "use strict";
 
 var SPHash = class SPHash {
- constructor(bounds) {
-   this.bounds = bounds;
-  this.maxX = bounds.x + bounds.width
-  this.maxY = bounds.y + bounds.width
+ constructor(bound) {
+   this.bounds = bound
+   
  }
   
   
