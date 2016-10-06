f2530c128543ed1a03ebfba670c94f40480261a5
Andrew54757 <andrews54757@gmail.com>
2016-10-06 12:14:27 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,11 +1,23 @@
 "use strict";
-
-var SPHash = class SPHash {
- constructor(bound) {
-   this.bounds = bound
-   
+var Bucket = class Bucket {
+ constructor(min,max,main,hash) {
+  this.min = min;
+  this.max = max;
+  this.main = main;
+  this.hash = hash
  }
-  
+ 
+}
+var SPHash = class SPHash {
+ constructor(bound,buckets) {
+   this.bounds = bound
+   this.amount = buckets
+  this.buckets = {}; 
+  this.createBuckets()
+ }
+  createBuckets() {
+   var a = this.bounds.x 
+  }
   
 }
 module.exports = SPHash
