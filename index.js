ffd991c6e5ddacfdc80c0988fe61e840918baaa2
Andrew S <andrews54757@gmail.com>
2016-12-14 13:10:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,42 @@
 */
 
 var Grid = require('./Grid.js')
+Map.prototype.every = function (c) {
+    var a = this.entries()
+    var b;
+    while (b = a.next().value) {
+        if (!c(b[1], b[0])) return true;
+    }
+  return false;
+}
+Map.prototype.toArray = function () {
+    var array = [];
+    this.forEach(function (a) {
+        array.push(a)
+    })
+    return array
+}
+Map.prototype.map = function(c) {
+  var f = new Map();
+ var a = this.entries()
+    var b;
+    while (b = a.next().value) {
+        f.set(b[0],c(b[1], b[0])) 
+    }
+  return f;
+  
+}
+Map.prototype.filter = function(c) {
+  var f = new Map();
+ var a = this.entries()
+    var b;
+    while (b = a.next().value) {
+        if (c(b[1], b[0])) f.set(b[0],b[1])
+    }
+  return f;
+  
+}
+
 module.exports = class HashBounds {
     constructor(power, lvl) {
         this.INITIAL = power;
