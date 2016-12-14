cc48b1d8ac2e0245a5547a991209996f2bccec59
Andrew S <andrews54757@gmail.com>
2016-12-14 10:37:12 -0500

    Update

---
+++ Grid.js
@@ -0,0 +1,130 @@
+"use strict"
+/*
+       HashBounds - A hierarchical spacial hashing system
+    Copyright (C) 2016 Andrew S
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+module.exports = class Grid {
+    constructor(g, p) {
+        this.SIZE = g;
+        this.LEVEL = p;
+        this.DATA = {};
+        this.LENGTH = 0;
+    }
+    getKey(x, y) {
+        return {
+            x: Math.floor(x / this.SIZE) * this.SIZE,
+            y: Math.floor(y / this.SIZE) * this.SIZE
+        }
+    }
+    insert(node) {
+
+        var a = this.getKey(node.bounds.width, node.bounds.height);
+        if (a.x + a.y >= 3 || this.LEVEL == 0) return false;
+        this.LENGTH++;
+        var x1 = node.bounds.x,
+            y1 = node.bounds.y,
+            x2 = node.bounds.x + node.bounds.width,
+            y2 = node.bounds.y + node.bounds.height;
+
+        var k1 = this.getKey(x1, y1)
+        var k2 = this.getKey(x2, y2)
+        node.hash.k1 = k1
+        node.hash.k2 = k2
+        node.hash.level = this.LEVEL
+        for (var i = k1.y; i < k2.y + 1; i++) {
+            for (var j = k1.x; j < k2.x + 1; j++) {
+                var ke = j + ":" + i;
+                if (!this.DATA[ke]) this.DATA[ke] = new Map()
+                this.DATA[ke].set(node.hash.id, node)
+            }
+
+        }
+        return true;
+    }
+    delete(node) {
+        var k1 = node.hash.k1
+        var k2 = node.hash.k2
+        this.LENGTH--;
+        for (var i = k1.y; i < k2.y + 1; i++) {
+            for (var j = k1.x; j < k2.x + 1; j++) {
+                var ke = j + ":" + i;
+
+                this.DATA[ke].delete(node.hash.id)
+            }
+
+        }
+    }
+    toArray(array, bounds) {
+        if (this.LENGTH <= 0) return;
+        var x1 = bounds.x,
+            y1 = bounds.y,
+            x2 = bounds.x + bounds.width,
+            y2 = bounds.y + bounds.height;
+
+        var k1 = this.getKey(x1, y1)
+        var k2 = this.getKey(x2, y2)
+        for (var i = k1.y; i < k2.y + 1; i++) {
+            for (var j = k1.x; j < k2.x + 1; j++) {
+                var ke = j + ":" + i;
+
+                if (this.DATA[ke]) this.DATA[ke].forEach((node) => {
+                    array.push(node)
+                })
+            }
+
+        }
+    }
+    every(bounds, call) {
+        if (this.LENGTH <= 0) return true;
+        var x1 = bounds.x,
+            y1 = bounds.y,
+            x2 = bounds.x + bounds.width,
+            y2 = bounds.y + bounds.height;
+
+        var k1 = this.getKey(x1, y1)
+        var k2 = this.getKey(x2, y2)
+        for (var i = k1.y; i < k2.y + 1; i++) {
+            for (var j = k1.x; j < k2.x + 1; j++) {
+                var ke = j + ":" + i;
+
+                if (this.DATA[ke])
+                    if (!this.DATA[ke].every(call)) return false;
+            }
+
+        }
+        return true;
+    }
+    forEach(bounds, call) {
+        if (this.LENGTH <= 0) return;
+        var x1 = bounds.x,
+            y1 = bounds.y,
+            x2 = bounds.x + bounds.width,
+            y2 = bounds.y + bounds.height;
+
+        var k1 = this.getKey(x1, y1)
+        var k2 = this.getKey(x2, y2)
+        for (var i = k1.y; i < k2.y + 1; i++) {
+            for (var j = k1.x; j < k2.x + 1; j++) {
+                var ke = j + ":" + i;
+
+                if (this.DATA[ke])
+                    this.DATA[ke].forEach(call)
+            }
+
+        }
+
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict"
 /*
-       HashBounds - A bound based spacial hashing system
+       HashBounds - A hierarchical spacial hashing system
     Copyright (C) 2016 Andrew S
 
     This program is free software: you can redistribute it and/or modify
@@ -17,232 +17,59 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var HashBounds = class HashBounds {
-    constructor(size, pn) {
-        this.size = size;
-        this.map = {};
-        this.length = 0;
-        this.key = 0;
-        this.pn = pn;
-        this.data = {}
+var Grid = require('./Grid.js')
+module.exports = class HashBounds {
+    constructor(inithash, lvl) {
+        this.INITIAL = inithash;
+        this.LVL = lvl;
+        this.LEVELS = []
+        this.lastid = 0;
+        this.createLevels()
 
+    }
+    createLevels() {
+        this.LEVELS = [];
+        this.LEVELS.push(new Grid(this.INITIAL, 0))
+        var a = this.INITIAL;
+        for (var i = 0; i < this.LVL; i++) {
+            a = a / 2
+            this.LEVELS.push(new Grid(a), i + 1)
+        }
+    }
+    insert(node) {
+        if (node.hash) throw "ERR: A node cannot be already in a hash!"
+        var bounds = node.bounds;
+        node.hash = {
+            id: this.lastid++,
+        }
+        var len = this.LEVELS.length
+        for (var i = 0; i < len; i++) {
+            if (this.LEVELS[len - i - 1].insert(node)) break;
+        }
     }
 
 
-    getNxt() {
-        return this.key++;
+    delete(node) {
+        if (!node.hash) throw "ERR: Node is not in a hash!"
+        this.LEVELS[node.hash.level].delete(node)
+        node.hash = null;
     }
-
     toArray(bounds) {
-        if (!bounds) throw "Bounds not specified"
-        var p1 = {
-            x: bounds.x,
-            y: bounds.y
+        var array = [];
+        for (var i = 0; i < this.LEVELS.length; i++) {
+            this.LEVELS[i].toArray(array, bounds)
         }
-        var p2 = {
-            x: bounds.x + bounds.width,
-            y: bounds.y + bounds.height
-        }
-        var a = this.getKey(p1)
-        var b = this.getKey(p2)
-
-        var table = {}; // hashtable
-        var result = [];
-        for (var i = a.y; i < b.y + 1; i++) {
-            for (var j = a.x; j < b.x + 1; j++) {
-                var ke = j + ":" + i;
-                if (this.map[ke]) {
-                    // n += ke + "|" + this.map[ke].length + ","
-
-                    this.map[ke].forEach((node) => {
-
-                        if (table[node.key]) return;
-
-                        result.push(this.data[node.key])
-                        table[node.key] = true;
-                    })
-
-                }
-            }
-
-        }
-
-        return result
-    }
-    forEach(bounds, call) {
-        if (!bounds) throw "Bounds not specified"
-        var p1 = {
-            x: bounds.x,
-            y: bounds.y
-        }
-        var p2 = {
-            x: bounds.x + bounds.width,
-            y: bounds.y + bounds.height
-        }
-        var a = this.getKey(p1)
-        var b = this.getKey(p2)
-
-        var table = {}; // hashtable
-
-        for (var i = a.y; i < b.y + 1; i++) {
-            for (var j = a.x; j < b.x + 1; j++) {
-                var ke = j + ":" + i;
-                if (this.map[ke]) {
-                    // n += ke + "|" + this.map[ke].length + ","
-
-                    this.map[ke].forEach((node) => {
-
-                        if (table[node.key]) return;
-
-                        call(this.data[node.key], node.key)
-                        table[node.key] = true;
-                    })
-
-                }
-            }
-
-        }
+        return array;
     }
     every(bounds, call) {
-        if (!bounds) throw "Bounds not specified"
-        var p1 = {
-            x: bounds.x,
-            y: bounds.y
+        for (var i = 0; i < this.LEVELS.length; i++) {
+            if (!this.LEVELS[i].every(bounds, call)) break;
         }
-        var p2 = {
-            x: bounds.x + bounds.width,
-            y: bounds.y + bounds.height
-        }
-        var a = this.getKey(p1)
-        var b = this.getKey(p2)
-
-        var table = {}; // hashtable
-
-        for (var i = a.y; i < b.y + 1; i++) {
-            for (var j = a.x; j < b.x + 1; j++) {
-                var ke = j + ":" + i;
-                if (this.map[ke]) {
-                    // n += ke + "|" + this.map[ke].length + ","
-
-                    if (!this.map[ke].every((node) => {
-
-                            if (table[node.key]) return true;
-
-                            return call(this.data[node.key], node.key)
-                            table[node.key] = true;
-                        })) break;
-
-                }
-            }
-
+    }
+    forEach(bounds, call) {
+        for (var i = 0; i < this.LEVELS.length; i++) {
+            this.LEVELS[i].forEach(bounds, call)
         }
     }
 
-    getKey(xy) {
-        return {
-            x: ~~(xy.x / this.size),
-            y: ~~(xy.y / this.size)
-        }
-    }
-    delete(node) {
-        if (!node.hash) return false;
-        this.length--;
-        var a = node.hash.a
-        var b = node.hash.b
-        if (!a || !b) return false;
-
-        for (var i = a.y; i < b.y + 1; i++) {
-            for (var j = a.x; j < b.x + 1; j++) {
-                var ke = j + ":" + i;
-                if (this.map[ke]) {
-                    if (this.map[ke][node.hash.key]) this.map[ke].delete(node.hash.key)
-                }
-            }
-
-        }
-        this.data[node.hash.key] = false
-        node.hash = false;
-        return true;
-    }
-
-
-    update(node) {
-        if (!node.hash) return false;
-        var a = node.hash.a
-        var b = node.hash.b
-        var p1 = {
-            x: node.bounds.x,
-            y: node.bounds.y
-        }
-        var p2 = {
-            x: node.bounds.x + node.bounds.width,
-            y: node.bounds.y + node.bounds.height
-        }
-
-        var c = this.getKey(p1)
-        var d = this.getKey(p2)
-
-        if (c == a && d == b) return
-        for (var i = a.y; i < b.y + 1; i++) {
-            for (var j = a.x; j < b.x + 1; j++) {
-
-                var ke = j + ":" + i;
-                if (this.map[ke]) {
-                    if (this.map[ke][node.hash.key]) this.map[ke].delete(node.hash.key)
-                }
-
-            }
-
-        }
-
-        this.insert(node, true)
-        return true;
-    }
-
-    insert(node, a) {
-        if (!a) this.length++;
-        if (this.pn) {
-            node.bounds = {
-                x: node.position.x - node.size,
-                y: node.position.y - node.size,
-                width: node.size * 2,
-                height: node.size * 2
-            }
-        }
-
-        var p1 = {
-            x: node.bounds.x,
-            y: node.bounds.y
-        }
-        var p2 = {
-            x: node.bounds.x + node.bounds.width,
-            y: node.bounds.y + node.bounds.height
-        }
-
-        var a = this.getKey(p1)
-        var b = this.getKey(p2)
-
-        var key = (node.hash && node.hash.key) ? node.hash.key : this.getNxt()
-        node.hash = {
-
-            a: a,
-            b: b,
-            key: key,
-            bounds: node.bounds
-
-        }
-
-        this.data[key] = node
-
-        for (var i = a.y; i < b.y + 1; i++) {
-            for (var j = a.x; j < b.x + 1; j++) {
-                var ke = j + ":" + i;
-
-                if (!this.map[ke]) this.map[ke] = new Map();
-                this.map[ke].set(node.hash.key, node.hash)
-            }
-
-        }
-    }
 }
-module.exports = HashBounds
