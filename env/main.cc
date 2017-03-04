cbe2882aed77fc26924ece8754eb1148b45c99c3
Andrew S <andrews54757@gmail.com>
2017-03-04 18:29:41 -0500

    Update main.cc

--- env/main.cc
+++ env/main.cc
@@ -1,1 +1,35 @@
+#include <node.h>
 
+class LinkedListRoot {
+  public:
+     int PARENT;
+     void forEach(int);
+     int every(int);
+  
+}
+void LinkedListRoot::forEach(int) {
+  
+}
+int LinkedListRoot::every(int) {
+  
+  return 1;
+}
+
+
+class LinkedListNode {
+  public:
+     int PARENT;
+     int CHILD;
+     void forEach(int);
+     int every(int);
+     void destroy(void)
+  
+}
+void LinkedListRoot::forEach(int) {
+  
+}
+int LinkedListRoot::every(int) {
+  
+  return 1;
+}
+int
