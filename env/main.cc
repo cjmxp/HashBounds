3dd2dbd3217a45f47b92596bc5aeb9edac27f0e2
Andrew S <andrews54757@gmail.com>
2017-03-04 19:47:15 -0500

    Update main.cc

--- env/main.cc
+++ env/main.cc
@@ -1,35 +1,61 @@
 #include <node.h>
 
-class LinkedListRoot {
-  public:
-     int PARENT;
-     void forEach(int);
-     int every(int);
-  
-}
-void LinkedListRoot::forEach(int) {
-  
-}
-int LinkedListRoot::every(int) {
-  
-  return 1;
-}
 
 
 class LinkedListNode {
   public:
-     int PARENT;
-     int CHILD;
+     LinkedListNode* CHILD;
+     int NODE;
      void forEach(int);
      int every(int);
-     void destroy(void)
+     void destroy(void);
   
 }
-void LinkedListRoot::forEach(int) {
+void LinkedListNode::forEach(int (*call)(int)) {
+  call(NODE);
+    if (CHILD) CHILD->call(call);
+}
+int LinkedListNode::every(int (*call)(int)) {
+  if (!call(NODE)) return 0;
+  
+  if (!CHILD) return 1;
+  
+    return CHILD->call(call);
+}
+void LinkedListNode::remove(int node) {
+  if (CHILD && CHILD->NODE == node) {
+    CHILD = CHILD->CHILD; 
+  } else {
+   CHILD->remove(node);
+  }
   
 }
-int LinkedListRoot::every(int) {
-  
-  return 1;
+class LinkedList {
+ public:
+  LinkedListNode* CHILD;
+  void set(int);
+  void forEach(int);
+  void every(int);
+  void remove(int);
+  init();
+    
 }
-int
+void LinkedList::insert(int val) {
+  LinkedListNode node;
+  node.NODE = val;
+  if (LinkedListNode) node.CHILD = &LinkedListNode;
+  CHILD = &node;
+}
+void LinkedList::forEach(int call) {
+ if (CHILD) CHILD->forEach(call);
+}
+int LinkedList::every(int call) {
+  if (CHILD) return CHILD->forEach(call); else return 1;
+}
+void LinkedList::remove(int node) {
+  if (CHILD && CHILD->NODE == node) {
+    CHILD = CHILD->CHILD; 
+  } else {
+   CHILD->remove(node);
+  }
+}
