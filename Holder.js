3523ef211235dff9cbea3462b845ebe207157a15
Andrew S <andrews54757@gmail.com>
2017-06-03 14:27:59 -0400

    Update Holder.js

--- Holder.js
+++ Holder.js
@@ -13,7 +13,6 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-var QuickMapV2 = require('./QuickMapV2.js')
 module.exports = class Holder {
     constructor(parent, x, y, power, lvl) {
         this.PARENT = parent;
