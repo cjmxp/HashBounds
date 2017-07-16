7d89817e80e6a78e81d3a30f36f7a13bab5f41b3
Andrew S <andrews54757@gmail.com>
2017-07-15 20:14:06 -0400

    Typo

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ module.exports = class HashBounds {
         if (bounds.TYPE === undefined) {
             if (bounds.x !== undefined) {
                 this.psToMM(bounds);
-                bounds.type = 1;
+                bounds.TYPE = 1;
             } else {
                 this.mmToPs(bounds);
                 bounds.TYPE = 2;
